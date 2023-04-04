9e301989438636ffd56c03361005d2c47e99c54c
nct031194 <nct031194@icloud.com>
2023-04-04 08:55:16 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int i = 10; // khai báo biến i là kiểu int có giá trị là 10
+        float f = 20.5f; // Khai báo biến f có kiểu là float có giá trị là 20.5
+        double d = 20.5; // Khai báo biến d có kiểu là double có giá trị là 20.5
+        boolean b = true; // Khai báo biến b là kiểu boolean có giá trị là true
+        char c = 'a'; // Khai báo biến c có kiểu là char có giá trị là kí tự a
+        String s = "Hà Nội"; //Khai báo biến s có kiểu là String có giá trị là chuỗi Hà Nội
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
     }
 }
