6d2b6c1e588d298a524a21a52fa917ce8c1860e8
nct031194 <nct031194@icloud.com>
2023-04-11 15:53:11 +0700

    upload

--- src/SwapArray/SwapArray.java
+++ src/SwapArray/SwapArray.java
@@ -1,2 +1,36 @@
-package SwapArray;public class SwapArray {
+package SwapArray;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class SwapArray {
+
+    static void reservearray(int a[]){
+        int[] b = new int [a.length];
+        int j = a.length;
+        for (int i = 0; i < a.length ; i++){
+            b[i] = a[j-1];
+            j--;
+            System.out.print(b[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int size;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array :");
+        while(!scanner.hasNextInt()){
+            System.out.println("Invalid number ");
+            System.out.println("enter again...");
+            scanner.next();
+        }
+        size = scanner.nextInt();
+        int [] array = new int[size];
+        System.out.println("Elements of array:");
+        for (int i = 0 ; i < array.length ; i++) {
+            array[i] = (int) (Math.random() * 20);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\n Reserve array :");
+        reservearray(array);
+
+    }
 }
--- src/test.java
+++ src/test.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class test {
+public class test {
+    public static void main(String[] args) {
+        int a = 2 < 3 ? 2 : 3;
+        switch(a) {
+            case 1:
+                System.out.print("A");
+                break;
+            case 2:
+                System.out.print("B");
+            default:
+                System.out.print("C");
+        }
+    }
 }
